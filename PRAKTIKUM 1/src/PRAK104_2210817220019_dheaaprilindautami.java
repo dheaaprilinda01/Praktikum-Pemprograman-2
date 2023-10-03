4242dd3c99b4b87fef0d2077a9ee57288915fa00
dheaaprilinda01 <114895343+dheaaprilinda01@users.noreply.github.com>
2023-10-03 21:59:09 +0800

    Delete PRAKTIKUM 1 directory

--- PRAKTIKUM 1/src/PRAK104_2210817220019_dheaaprilindautami.java
+++
@@ -1,8 +0,0 @@
-public class PRAK104_2210817220019_dheaaprilindautami {
-    public static void main(String[] args){
-        String nama= "Dhea Aprilinda Utami";
-        int umur= 20;
-
-        System.out.printf("Perkenalkan, Nama Saya " + nama + ", Umur Saya " + umur);
-    }
-}
